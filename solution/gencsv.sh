2f7a3698fc231117a35e1eb34b48b797eafbf5a1
Sai8kumar <118273890+Sai8kumar@users.noreply.github.com>
2022-12-13 23:42:37 +0530

    Update gencsv.sh

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-read -r $RANDOM
-i=1
-while [ $i -le 10 ]
-do
-b=`expr $c \* $i`
-echo " $i, $RANDOM"
-i=`expr $i + 1`
-done
+
+# Set the number of entries to generate
+num_entries=10
+
+# Check if a number of entries was provided as an argument
+if [ $# -eq 1 ]; then
+            num_entries=$1
+fi
+
+# Generate the file
+for i in $(seq 0 $(($num_entries - 1))); do
+            echo "$i, $((RANDOM % 100))" >> inputFile
+    done
